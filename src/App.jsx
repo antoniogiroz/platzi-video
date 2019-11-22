9c60f6a2f00564501df32272c2e2e5746ef340c2
Antonio Gil <antonioluisgil@gmail.com>
2019-11-22 21:41:14 +0100

    Install react dependencies and dev environment with webpack

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './assets/scss/main.scss';
+
+export default class App extends React.Component {
+  render() {
+    return <div className="app">Hola Mundo</div>;
+  }
+}
